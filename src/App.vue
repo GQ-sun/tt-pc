4eafbfb6597a128c402f6f50fc23c48d2107a30e
GQ-sun <genjiujiu@163.com>
2019-10-25 16:49:39 +0800

    登录页面完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app">tt-pc</div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
---
+++ src/views/login/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <el-card>
+      <img src="../../assets/logo_index.png" alt style="width:200px" />
+      <!-- 表单部分 -->
+      <el-form ref="form" :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;marginRight:8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">立即登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style  scoped lang='less'>
+.container {
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/login_bg.jpg") no-repeat center / cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.el-card {
+  width: 400px;
+  height: 340px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  img {
+    display: block;
+    margin: 0 auto 30px;
+  }
+}
+</style>
