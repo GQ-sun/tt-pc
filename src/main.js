d9099adc4e6d0a2f811dd54ff0cfab55a57f0600
GQ-sun <genjiujiu@163.com>
2019-10-26 15:34:15 +0800

    登录界面校验完成

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import router from '@/router'// 绝对路径
+
+import axios from '@/api'
+Vue.prototype.$http = axios
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,11 +3,11 @@
     <el-card>
       <img src="../../assets/logo_index.png" alt style="width:200px" />
       <!-- 表单部分 -->
-      <el-form ref="form" :model="loginForm">
-        <el-form-item>
+      <el-form ref="loginForm" :model="loginForm" :rules="rules">
+        <el-form-item prop="mobile">
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input
             v-model="loginForm.code"
             placeholder="请输入验证码"
@@ -19,7 +19,7 @@
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">立即登录</el-button>
+          <el-button type="primary" style="width:100%" @click="login">立即登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -29,11 +29,49 @@
 <script>
 export default {
   data () {
+    const checkMobile = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不对'))
+      }
+    }
     return {
       loginForm: {
         mobile: '',
         code: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          {
+            validator: checkMobile,
+            trigger: 'blur'
+          }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码长度为6位数', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs['loginForm'].validate(valid => {
+        if (valid) {
+          // 发请求校验手机号和验证码
+          this.$http
+            .post('authorizations', this.loginForm)
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 失败提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
     }
   }
 }
@@ -43,7 +81,7 @@ export default {
 .container {
   width: 100%;
   height: 100%;
-  background: url("../../assets/login_bg.jpg") no-repeat center / cover;
+  background: url('../../assets/login_bg.jpg') no-repeat center / cover;
   position: absolute;
   top: 0;
   left: 0;
